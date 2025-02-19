c55370d4afc8bf549cca3940a75e58205ce4e06c
SittikornFirst <sittikornsfirst@gmail.com>
2025-02-19 21:39:12 +0700

    Added Java Class Hodiday Travel Vehicles

---
+++ HolidayTravelVehicles/Vehicle.java
@@ -0,0 +1,69 @@
+class Vehicle {
+    private String vehicleSerialNumber;
+    private String name;
+    private String model;
+    private int year;
+    private String manufacturer;
+    private double baseCost;
+
+    public Vehicle(String vehicleSerialNumber, String name, String model, int year, String manufacturer, double baseCost) {
+        this.vehicleSerialNumber = vehicleSerialNumber;
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.manufacturer = manufacturer;
+        this.baseCost = baseCost;
+    }
+
+    public void createNewVehicleRecord() {
+        
+    }
+
+    public String getVehicleSerialNumber() {
+        return vehicleSerialNumber;
+    }
+
+    public void setVehicleSerialNumber(String vehicleSerialNumber) {
+        this.vehicleSerialNumber = vehicleSerialNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public double getBaseCost() {
+        return baseCost;
+    }
+
+    public void setBaseCost(double baseCost) {
+        this.baseCost = baseCost;
+    }
+}
