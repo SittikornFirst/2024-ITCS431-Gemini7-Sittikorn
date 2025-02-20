20885dae18a63c1959809521d12292dc2dbaf5f1
JaleNemo <109641106+JaleNemo@users.noreply.github.com>
2025-02-20 21:09:00 +0700

    TravelVehicles 01

---
+++ HolidayTravelVehicles/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("8998", "Audi", "RS3", 2024, "Audi", 5390000.00);
+        vehicle.displayInfo();
+    }
+}
