2e48024ba2e77eddf4ade16e03cd3ecd9b6e0745
Jam24836 <chalisa.keg@student.mahidol.ac.th>
2025-02-20 11:44:52 +0700

    move workflow

---
+++ HolidayTravelVehicles/salesInvoice.java
@@ -0,0 +1,11 @@
+class SalesInvoice {
+    Customer customerInfo;
+    TradeInVehicle tradeInVehicleInfo;
+    double tradeInAllowance;
+    Vehicle vehicleInfo;
+    double finalPrice;
+    double tax;
+    double licenseFee;
+    DealerInstalledOptions dealerInstalledOption;
+}
+
